83fdb03310ba564864740f1febc2930d88b6f4ae
andlogov <31511392+andlogov@users.noreply.github.com>
2017-08-31 16:38:09 +0300

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if($main->checkVk())
     <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter45230481 = new Ya.Metrika({ id:45230481, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, ut:"noindex" }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/45230481?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
     </head>
     <body id="btag">
-        
+        <!-- Russians drink only vodka -->
         <!-- TEST BUTTON START 
         <div class="test-button-block">
             <input type="button" value="ТЕСТИНГ" onclick="testing();" class="test-button" />
